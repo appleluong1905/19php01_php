a43893ea081d2dfa91d17b70c840f003b3bc6243
Chad Luong <apple.luong1905@gmail.com>
2019-06-07 20:17:21 +0700

    update

--- session22/register2/register.php
+++ session22/register2/register.php
@@ -161,12 +161,81 @@
         <li class="active">Dashboard</li>
       </ol>
     </section>
-
+    <?php 
+      $errClassName = $errClassEmail = '';
+      $errTextName = $errTextEmail = '';
+      $name = $emai = '';
+      if (isset($_POST['register'])) {
+        $name  = $_POST['name'];
+        $email = $_POST['email'];
+        if ($name == '') {
+          $errClassName = 'has-error';
+          $errTextName = 'Please input your name';
+        }
+      }
+    ?>
     <!-- Main content -->
     <section class="content">
-      <!-- Info boxes -->
-        Content here!
-      <!-- /.row -->
+      <div class="row">
+        <!-- left column -->
+        <div class="col-md-6">
+          <!-- general form elements -->
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">Register form</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form" action="#" method="POST" enctype="multipart/form-data">
+              <div class="box-body">
+                <div class="form-group <?php echo $errClassName;?>">
+                  <label for="exampleInputEmail1">Name</label>
+                  <input type="text" class="form-control" id="exampleInputName" placeholder="Enter name" name="name" value="<?php echo $name;?>">
+                  <span class="help-block"><?php echo $errTextName;?></span>
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Email</label>
+                  <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Phone</label>
+                  <input type="text" class="form-control" id="exampleInputPhone" placeholder="Enter phone">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputFile">Avatar</label>
+                  <input type="file" id="exampleInputFile">
+                </div>
+                <!-- radio -->
+                <div class="form-group">
+                  <label>
+                    <input type="radio" name="r1" class="minimal" checked>Male
+                  </label>
+                  <label>
+                    <input type="radio" name="r1" class="minimal">Female
+                  </label>
+                  <label>
+                    <input type="radio" name="r1" class="minimal">Other
+                </div>
+              </div>
+              <div class="form-group">
+                <label>City</label>
+                <select class="form-control select2" style="width: 100%;">
+                  <option value="">Choose city</option>
+                  <option value="Alaska">Alaska</option>
+                  <option value="California">California</option>
+                  <option value="Delaware">Delaware</option>
+                </select>
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary" name="register">Submit</button>
+              </div>
+            </form>
+          </div>
+          <!-- /.box -->
+        </div>
+      </div>
     </section>
     <!-- /.content -->
   </div>
