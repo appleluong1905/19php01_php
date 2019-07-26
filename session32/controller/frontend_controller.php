5499b9de15891ff48fe09b804c3940a1338e8785
Chad Luong <apple.luong1905@gmail.com>
2019-07-26 20:13:25 +0700

    update

--- session32/controller/frontend_controller.php
+++ session32/controller/frontend_controller.php
@@ -4,6 +4,7 @@
 		function handleRequestFrontend() {
 			$controller = isset($_GET['controller'])?$_GET['controller']:'home_page';
 			$action = isset($_GET['action'])?$_GET['action']:'list_product';
+			echo "o day chua?";
 		}
 
 	}
